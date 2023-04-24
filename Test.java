8dbf228802c8cbcdf9ddcfbe1b35e143fd7db9c9
arifhamidan <arifhamidan@gmail.com>
2023-04-24 23:47:39 +0700

    update file test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,7 @@
 public class Test{
     public static void main(String[] args) {
+        String repo ="https://github.com/arifhamidan/Test-Git.git";
         System.out.println("Hello ini file class test.java");
+        System.out.println("Saya menyimpan nya pada git dengan url repo "+ repo );
     }
 }
